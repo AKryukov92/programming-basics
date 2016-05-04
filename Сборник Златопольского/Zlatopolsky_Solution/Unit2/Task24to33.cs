3ecd742ab4d948c3419a2cd59973b95305e5defe
Alexander Kryukov <akryukov@luxoft.com>
2016-05-04 19:05:41 +0600

    Первая лабораторная работа разбита на четыре. Добавлены задачи к лр 1.3 на операцию "остаток от деления".

---
+++ Сборник Златопольского/Zlatopolsky_Solution/Unit2/Task24to33.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Unit2
+{
+    public class Task24to33
+    {
+
+        public void task25()
+        {
+            string temp;
+            temp = Console.ReadLine();
+            int n;
+            int x;
+            n = int.Parse(temp);
+            if (n < 100)
+            {
+                Console.WriteLine("Введенное число не подходит для решения задачи.");
+                return;
+            }
+            if (n >= 1000)
+            {
+                Console.WriteLine("Введенное число не подходит для решения задачи.");
+                return;
+            }
+            int step2, step1;
+            step2 = n / 100;
+            step1 = n % 100 * 10;
+            x = step1 + step2;
+            Console.WriteLine(x);
+        }
+
+        public void task25rev()
+        {
+            string temp;
+            temp = Console.ReadLine();
+            int x;
+            int n;
+            x = int.Parse(temp);
+            if (x < 100)
+            {
+                Console.WriteLine("Введенное число не подходит для решения задачи.");
+                return;
+            }
+            if (x >= 1000)
+            {
+                Console.WriteLine("Введенное число не подходит для решения задачи.");
+                return;
+            }
+            n = x - x % 10;
+            n = n / 10;
+            n = n + (x % 10) * 100;
+            Console.WriteLine(n);
+        }
+
+        public void task26()
+        {
+            string temp;
+            temp = Console.ReadLine();
+            int n;
+            int x;
+            n = int.Parse(temp);
+            if (n < 100)
+            {
+                Console.WriteLine("Введенное число не подходит для решения задачи.");
+                return;
+            }
+            if (n >= 1000)
+            {
+                Console.WriteLine("Введенное число не подходит для решения задачи.");
+                return;
+            }
+            int step1, step2;
+            step1 = n % 10;
+            step2 = n / 10;
+            x = step2 + step1 * 100;
+            Console.WriteLine(x);
+        }
+
+        public void task26rev()
+        {
+            string temp;
+            temp = Console.ReadLine();
+            int n;
+            int x;
+            x = int.Parse(temp);
+            if (x < 100)
+            {
+                Console.WriteLine("Введенное число не подходит для решения задачи.");
+                return;
+            }
+            if (x >= 1000)
+            {
+                Console.WriteLine("Введенное число не подходит для решения задачи.");
+                return;
+            }
+            n = x % 100;
+            n = n * 10;
+            n = n + x / 100;
+            Console.WriteLine(n);
+        }
+
+        public void task27rev()
+        {
+            string temp;
+            temp = Console.ReadLine();
+            int n;
+            int x;
+            x = int.Parse(temp);
+            if (x < 100)
+            {
+                Console.WriteLine("Введенное число не подходит для решения задачи.");
+                return;
+            }
+            if (x >= 1000)
+            {
+                Console.WriteLine("Введенное число не подходит для решения задачи.");
+                return;
+            }
+            n = x % 10 + x / 100 * 10;
+            n = n + (x / 10 % 10) * 100;
+            Console.WriteLine(n);
+        }
+    }
+}
---
+++ Сборник Златопольского/Zlatopolsky_Solution/Unit2Test/Task24to33Tests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Unit2;
+using Unit2Tests;
+
+namespace Unit2Test
+{
+    [TestClass()]
+    public class Task24to33Tests : ConsoleTest
+    {
+        Task24to33 obj = new Task24to33();
+
+        [TestMethod()]
+        public void task25Test()
+        {
+            FakeInput("237");
+            Begin();
+            obj.task25();
+            Assert.AreEqual("372", Output);
+        }
+
+        [TestMethod()]
+        public void task25revTest()
+        {
+            FakeInput("372");
+            Begin();
+            obj.task25rev();
+            Assert.AreEqual("237", Output);
+        }
+
+        [TestMethod()]
+        public void task26Test()
+        {
+            FakeInput("791");
+            Begin();
+            obj.task26();
+            Assert.AreEqual("179", Output);
+        }
+
+        [TestMethod()]
+        public void task26revTest()
+        {
+            FakeInput("179");
+            Begin();
+            obj.task26rev();
+            Assert.AreEqual("791", Output);
+        }
+
+        [TestMethod()]
+        public void task27revTest()
+        {
+            FakeInput("541");
+            Begin();
+            obj.task27rev();
+            Assert.AreEqual("451", Output);
+        }
+    }
+}
