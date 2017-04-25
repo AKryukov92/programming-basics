0c73b431b93b7f053b2b4c3578995639a0e10be5
Alexander Kryukov <akryukov992@gmail.com>
2017-04-25 13:04:32 +0600

    remove boring tasks from lab13 task9701

--- src/lab13/task9701/task9701caller.js
+++ src/lab13/task9701/task9701caller.js
@@ -1,73 +1,8 @@
-var in9298 = ["1","5","0","-2"];
-for (var i = 0; i < in9298.length; i++){
-	var out = document.getElementById("9298t" + i);
-	try {
-		out.innerHTML = task9298(in9298[i]);
-	} catch (e){
-		out.innerHTML = e.message;
-	}
-}
-var in3354 = ["7", "11", "0", "-3"];
-for (var i = 0; i < in3354.length; i++){
-	var out = document.getElementById("3354t" + i);
-	try {
-		out.innerHTML = task3354(in3354[i]).toFixed(4);
-	} catch (e){
-		out.innerHTML = e.message;
-	}
-}
-var in5201 = ["13", "17", "0", "-2"];
-for (var i = 0; i < in5201.length; i++){
-	var out = document.getElementById("5201t" + i);
-	try {
-		out.innerHTML = task5201(in5201[i]).toFixed(4);
-	} catch (e){
-		out.innerHTML = e.message;
-	}
-}
-var in2981 = ["19", "23", "0", "-7"];
-for (var i = 0; i < in2981.length; i++){
-	var out = document.getElementById("2981t" + i);
-	try {
-		out.innerHTML = task2981(in2981[i]).toFixed(4);
-	} catch (e){
-		out.innerHTML = e.message;
-	}
-}
-var in4312a = ["1","2","0","-2","3"];
-var in4312h = ["1","3","10","1","-4"];
-for (var i = 0; i < in4312a.length; i++){
-	var out = document.getElementById("4312t" + i);
-	try {
-		out.innerHTML = task4312(in4312a[i], in4312h[i]).toFixed(2);
-	} catch (e){
-		out.innerHTML = e.message;
-	}
-}
-var in8833 = ["7606", "9", "0", "-7603"];
-for (var i = 0; i < in8833.length; i++){
-	var out = document.getElementById("8833t" + i);
-	try {
-		out.innerHTML = task8833(in8833[i]).toFixed(4);
-	} catch (e){
-		out.innerHTML = e.message;
-	}
-}
 var in9020 = ["0", "-9", "61", "60", "62", "7573", "75"];
 for (var i = 0; i < in9020.length; i++){
 	var out = document.getElementById("9020t" + i);
 	try {
 		out.innerHTML = task9020(in9020[i]).toFixed(4);
-	} catch (e){
-		out.innerHTML = e.message;
-	}
-}
-var in1934a = ["6047","-3","0","13","6221"];
-var in1934b = ["-6011", "-7", "-50","0", "6373"];
-for (var i = 0; i < in1934a.length; i++){
-	var out = document.getElementById("1934t" + i);
-	try {
-		out.innerHTML = task1934(in1934a[i], in1934b[i]).toFixed(4);
 	} catch (e){
 		out.innerHTML = e.message;
 	}
@@ -91,50 +26,12 @@ for (var i = 0; i < in3943x.length; i++){
 		out.innerHTML = e.message;
 	}
 }
-var in7619v1 = ["31", "0", "-13", "67"];
-var in7619v2 = ["47", "7", "11", "-79"];
-var in7619s = ["6833", "6829", "200", "400"];
-for (var i = 0; i < in7619v1.length; i++){
-	var out = document.getElementById("7619t" + i);
-	try {
-		var r = task7619(in7619v1[i], in7619v2[i], in7619s[i]);
-		out.innerHTML = r.toFixed(4);
-	} catch (e){
-		out.innerHTML = e.message;
-	}
-}
 var in8873r1 = ["7331","3","0","7309"];
 var in8873r2 = ["7321","5","7307","0"];
 for (var i = 0; i < in8873r1.length; i++){
 	var out = document.getElementById("8873t" + i);
 	try {
 		out.innerHTML = task8873(in8873r1[i], in8873r2[i]).toFixed(4);
-	} catch (e){
-		out.innerHTML = e.message;
-	}
-}
-var in9130y = ["7211","11","7177"];
-var in9130m1 = ["7219","3","7193"];
-var in9130m2 = ["7213","7","7207"];
-var in9130r = ["7207","13","0"];
-for (var i = 0; i < in9130y.length; i++){
-	var out = document.getElementById("9130t" + i);
-	try {
-		var r = task9130(in9130y[i], in9130m1[i],in9130m2[i], in9130r[i]);
-		out.innerHTML = r.toFixed(4);
-	} catch (e){
-		out.innerHTML = e.message;
-	}
-}
-var in2461a = ["7069", "0", "7019"];
-var in2461b = ["7043", "7043", "7043"];
-var in2461c = ["7039", "7039","7001"];
-var in2461d = ["7057","7027","0"];
-for (var i = 0; i < in2461a.length; i++){
-	var out = document.getElementById("2461t" + i);
-	try {
-		var r = task2461(in2461a[i], in2461b[i],in2461c[i], in2461d[i]);
-		out.innerHTML = r.toFixed(4);
 	} catch (e){
 		out.innerHTML = e.message;
 	}
@@ -171,37 +68,6 @@ for (var i = 0; i < in3591.length; i++){
 		out.innerHTML = e.message;
 	}
 }
-var in7937w = ["1721","2473","0","877","1181","3491"];
-var in7937h = ["661","449","1879","0","1033","3533"];
-var in7937c = ["11","23","1163","7","0","1297"];
-var in7937d = ["7","11","109","19","127","0"];
-for (var i = 0; i < in7937w.length; i++){
-	var out = document.getElementById("7937t" + i);
-	try {
-		var r = task7937(in7937w[i], in7937h[i], in7937c[i], in7937d[i]);
-		out.innerHTML = r;
-	} catch(e){
-		out.innerHTML = e.message;
-	}
-}
-var in9180 = ["-100","7","368","501","-101"];
-for (var i = 0; i < in9180.length; i++){
-	var out = document.getElementById("9180t" + i);
-	try {
-		out.innerHTML = task9180(in9180[i]);
-	} catch(e){
-		out.innerHTML = e.message;
-	}
-}
-var in1544 = ["12","70","368","10000","-11","10001"];
-for (var i = 0; i < in1544.length; i++){
-	var out = document.getElementById("1544t" + i);
-	try {
-		out.innerHTML = task1544(in1544[i]);
-	} catch(e){
-		out.innerHTML = e.message;
-	}
-}
 var in2802 = ["4","11","19","-1","0"];
 for (var i = 0; i < in2802.length; i++){
 	var out = document.getElementById("2802t" + i);
@@ -230,16 +96,6 @@ for (var i = 0; i < in9812a.length; i++){
 		out.innerHTML = e.message;
 	}
 }
-var in4845a = ["0","5","2","-1","20","5","7"];
-var in4845b = ["3","9","7","4","4","-2","17"];
-for (var i = 0; i < in4845a.length; i++){
-	var out = document.getElementById("4845t" + i);
-	try {
-		out.innerHTML = task4845(in4845a[i], in4845b[i]);
-	} catch(e){
-		out.innerHTML = e.message;
-	}
-}
 var in5728a = ["a","c","y","g","a"];
 var in5728b = ["z","d","d","z","l"];
 for (var i = 0; i < in5728a.length; i++){
@@ -260,15 +116,6 @@ for (var i = 0; i < in5923a.length; i++){
 		out.innerHTML = e.message;
 	}
 }
-var in2166 = ["cd","aa","xy","abc","f"];
-for (var i = 0; i < in2166.length; i++){
-	var out = document.getElementById("2166t" + i);
-	try {
-		out.innerHTML = task2166(in2166[i]);
-	} catch(e){
-		out.innerHTML = e.message;
-	}
-}
 var in9116 = ['"   12345"', '"abcdef    "', '"  xyz  "',
 '"  a b  "', '" pq"rt "','asdf"','"xyz','p"w"'];
 for (var i = 0; i < in9116.length; i++){
@@ -284,16 +131,6 @@ for (var i = 0; i < in1618.length; i++){
 	var out = document.getElementById("1618t" + i);
 	try {
 		out.innerHTML = task1618(in1618[i]);
-	} catch(e){
-		out.innerHTML = e.message;
-	}
-}
-var in4425a = ["0","3","9","5","-1","0"];
-var in4425data = ["4 3 2 1", "4 3 2 1", "0 9 8 7 6 5 4 3 2 1", "4 3 2 1", "4 3 2 1", ""];
-for (var i = 0; i < in4425a.length; i++){
-	var out = document.getElementById("4425t" + i);
-	try {
-		out.innerHTML = task4425(in4425a[i], in4425data[i]);
 	} catch(e){
 		out.innerHTML = e.message;
 	}
--- src/lab13/task9701/task9701functions.js
+++ src/lab13/task9701/task9701functions.js
@@ -1,55 +1,10 @@
 var STR = "abcdefwxyz";
 var STR2 = "abcdacadbacdaabaadc";
-function task9298(a){
-	if (a <= 0){
-		throw new Error("Значение A должно быть положительным");
-	}
-	return 6 * a * a;
-}
-function task3354(a){
-	if (a <= 0){
-		throw new Error("Значение A должно быть положительным");
-	}
-	return a * a * a;
-}
-function task5201(r){
-	if (r <= 0){
-		throw new Error("Значение R должно быть положительным");
-	}
-	return 2 * Math.PI * r;
-}
-function task2981(r){
-	if (r <= 0){
-		throw new Error("Значение R должно быть положительным");
-	}
-	return 4 * Math.PI * r * r * r / 3;
-}
-function task4312(a,h){
-	if (a <= 0){
-		throw new Error("Значение A должно быть положительным");
-	}
-	if (h <= 0){
-		throw new Error("Значение H должно быть положительным");
-	}
-	return a * h / 2;
-}
-function task8833(x){
-	if (x < 0){
-		throw new Error("Значение X должно быть неотрицательным");
-	}
-	return Math.sqrt(x);
-}
 function task9020(x){
 	if (x > 61){
 		throw new Error("Подкоренное выражение должно быть неотрицательным");
 	}
 	return 3 * Math.sqrt(61-x);
-}
-function task1934(a,b){
-	if (b > 0){
-		throw new Error("Значение B должно быть не положительным");
-	}
-	return a*Math.sqrt(-7*b);
 }
 function task7237(tc){
 	if (tc < -273.15){
@@ -67,15 +22,6 @@ function task3943(x,y){
 	}
 	return -5 * Math.sqrt(t);
 }
-function task7619(v1, v2, s){
-	if (v1 < 0){
-		throw new Error("Значение v1 должно быть неотрицательным");
-	}
-	if (v2 < 0){
-		throw new Error("Значение v2 должно быть неотрицательным");
-	}
-	return s/(parseInt(v1)+parseInt(v2));
-}
 function task8873(r1, r2){
 	if (r1 == 0){
 		throw new Error("Значение r1 должно быть не равно нулю");
@@ -84,21 +30,6 @@ function task8873(r1, r2){
 		throw new Error("Значение r2 должно быть не равно нулю");
 	}
 	return 1/r1 + 1/r2;
-}
-function task9130(y,m1,m2,r){
-	if (r == 0){
-		throw new Error("Значение r должно быть не равно нулю");
-	}
-	return y*m1*m2/r/r;
-}
-function task2461(a,b,c,d){
-	if (a == 0){
-		throw new Error("Значение a должно быть не равно нулю");
-	}
-	if (d == 0){
-		throw new Error("Значение d должно быть не равно нулю");
-	}
-	return (a*d+b*c)/a/d;
 }
 function task2624(a,b,c,x){
 	var root = a * x * x + b * x + c;
@@ -125,51 +56,6 @@ function task3591(h){
 	var r = 6350;
 	var c = r+h;
 	return Math.sqrt(c*c-r*r);
-}
-function task7937(w,h,c,d){
-	if (w <= 0){
-		throw new Error("Значение W должно быть положительным");
-	}
-	if (h <= 0){
-		throw new Error("Значение H должно быть положительным");
-	}
-	if (c <= 0){
-		throw new Error("Значение C должно быть положительным");
-	}
-	if (d <= 0){
-		throw new Error("Значение D должно быть положительным");
-	}
-	var cw = (w/c)*(h/d);
-	var ch = (h/c)*(w/d);
-	if (cw > ch){
-		return cw.toFixed(4) + " при C вдоль W";
-	} else {
-		return ch.toFixed(4) + " при C вдоль H";
-	}
-}
-function task9180(a){
-	a = parseInt(a);
-	if (a < -100 || a > 500){
-		throw new Error("Значение A должно быть в интервале [-100, 500]");
-	}
-	var sum = 0;
-	while(a <= 500){
-		sum += a;
-		a++;
-	}
-	return sum;
-}
-function task1544(b){
-	b = parseInt(b);
-	if (b < -10 || b > 10000){
-		throw new Error("Значение b должно быть в интервале [-10,10000]");
-	}
-	var sum = 0;
-	while (b >= -10){
-		sum += b;
-		b--;
-	}
-	return sum;
 }
 function task2802(n){
 	if (n <= 0){
@@ -204,21 +90,6 @@ function task9812(a,b){
 		return ib - ia - 1;
 	}
 }
-function task4845(a,b){
-	a = parseInt(a);
-	b = parseInt(b);
-	if (a < 0 || a >= STR.length){
-		throw new Error("Значение A должно быть в интервале [0,длина строки)");
-	}
-	if (b < 0 || b >= STR.length){
-		throw new Error("Значение B должно быть в интервале [0,длина строки)");
-	}
-	if (a < b){
-		return STR.substring(a, b + 1);
-	} else {
-		return STR.substring(b, a + 1);
-	}
-}
 function task5728(a,b){
 	if (STR.indexOf(a) == -1){
 		throw new Error("Значение A отсутствует в строке");
@@ -247,12 +118,6 @@ function task5923(a,b){
 		throw new Error("Значение A должно быть в интервале [0,длина строки)");
 	}
 	return STR.substring(0,a) + STR.substring(a+b);
-}
-function task2166(x){
-	if (x.length != 2){
-		throw new Error("Вводимая строка должна содержать ровно 2 символа");
-	}
-	return STR2.replace(new RegExp(x,'g')," ");
 }
 function task9116(str){
 	if(!str.startsWith('"') || !str.endsWith('"')){
@@ -298,16 +163,6 @@ function task1618(line){
 		throw new Error(r + " Неожиданный конец строки");
 	}
 	return r;
-}
-function task4425(a, str){
-	if (str == ""){
-		throw new Error("Исходная строка пуста");
-	}
-	var arr = str.split(" ");
-	if (a < 0 || a >= arr.length){
-		throw new Error("Число A должно быть в интервале [0, размер массива)");
-	}
-	return arr[a];
 }
 function task7290(b, raw){
 	var arr = raw.split(" ");
