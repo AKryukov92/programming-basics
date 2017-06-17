eb62032632130ccb72aa80d3f5d7696df5076799
Alexander Kryukov <akryukov992@gmail.com>
2017-06-18 01:46:01 +0600

    add some theory for task 3862

--- src/lab01/make_cs01.bat
+++ src/lab01/make_cs01.bat
@@ -41,9 +41,7 @@ echo ^<div class="nav"^> ^
  ^</div^>>>%filename%
 
 type link_c1_p4.3.html>>%filename%
-type link_pre1662.html>>%filename%
 type task1662.html>>%filename%
-type link_pre1860.html>>%filename%
 type task1860.html>>%filename%
 type link_c7_p33.html>>%filename%
 type task4764.html>>%filename%
@@ -70,3 +68,4 @@ if x%1==x start "" %filename%
 cd ..
 call prepare_theory csharp 1662 cs lab01
 call prepare_theory csharp 1860 cs lab01
+call prepare_theory csharp 3862 cs lab01
