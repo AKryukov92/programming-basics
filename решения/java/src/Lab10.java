eacb6e058e3f7bb81a72b5b98561b0fe9628d5c1
Alexander Kryukov <akryukov992@gmail.com>
2017-09-11 12:20:05 +0600

    update test data in lab10

--- решения/java/src/Lab10.java
+++ решения/java/src/Lab10.java
@@ -12,46 +12,56 @@ public class Lab10 {
     private static final int VIEWPORT_HEIGHT = 600;
 
     public static void main(String[] args) {
-//        System.out.println("9300");
-//        int[] in9300cx = {0, 400, 600, 254, 400, 300, 100, 300, 600, 900};
-//        int[] in9300cy = {0, 300, 400, 356, 300, 300, 100, 250, 800, 200};
-//        int[] in9300r = {20, 200, 60, 100, 50, 0, 150, 80, 50, 51};
-//        int[] in9300strokeWidth = {5, 0, 10, 80, 100, 25, 20, 162, 100, 102};
-//        for (int i = 0; i < in9300cx.length; i++) {
-//            try {
-//                task9300(in9300cx[i], in9300cy[i], in9300r[i], in9300strokeWidth[i], i + 1);
-//            } catch (IllegalArgumentException ex) {
-//                System.out.println(ex.getMessage());
-//            }
-//        }
+        step6409();
+    }
 
-//        System.out.println("3951");
-//        int[] in3951x = {400, 135, 400, 200, 120, 200, 400, 1200, 601};
-//        int[] in3951y = {300, 100, 100, 200, 120, 200, 300, 600, 601};
-//        int[] in3951width = {250, 350, 200, 1, 0, 100, 420, 50, 51};
-//        int[] in3951height = {250, 100, 300, 100, 180, 0, 100, 100, 101};
-//        for (int i = 0; i < in3951x.length; i++) {
-//            try {
-//                task3951(in3951x[i], in3951y[i], in3951width[i], in3951height[i], i);
-//            } catch (IllegalArgumentException ex) {
-//                System.out.println(ex.getMessage());
-//            }
-//        }
+    private static void step9300() {
+        System.out.println("9300");
+        int[] in9300cx = {0, 400, 600, 254, 400, 300, 100, 300, 600, 900};
+        int[] in9300cy = {0, 300, 400, 356, 300, 300, 100, 250, 800, 200};
+        int[] in9300r = {20, 200, 60, 100, 50, 0, 150, 80, 50, 51};
+        int[] in9300strokeWidth = {5, 0, 10, 80, 100, 25, 20, 162, 100, 102};
+        for (int i = 0; i < in9300cx.length; i++) {
+            try {
+                task9300(in9300cx[i], in9300cy[i], in9300r[i], in9300strokeWidth[i], i + 1);
+            } catch (IllegalArgumentException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
 
-//        System.out.println("7343");
-//        int[] in7343x = {200, 150, 200, 250, 200, 700, 300, 25, 700, -10, 100};
-//        int[] in7343y = {200, 180, 200, 250, 120, 100, 300, 25, 500, 810, 650};
-//        int[] in7343width = {300, 120, 100, 0, 120, 150, 200, 100, 99, 100, 80};
-//        int[] in7343height = {100, 300, 100, 100, 0, 200, 200, 100, 102, 100, 80};
-//        int[] in7343r = {40, 50, 120, 20, 25, 20, 0, 50, 3, 100, 45};
-//        for (int i = 0; i < in7343x.length; i++) {
-//            try {
-//                task7343(in7343x[i], in7343y[i], in7343width[i], in7343height[i], in7343r[i], i);
-//            } catch (IllegalArgumentException ex) {
-//                System.out.println(ex.getMessage());
-//            }
-//        }
+    private static void step3951() {
+        System.out.println("3951");
+        int[] in3951x = {400, 135, 400, 200, 120, 200, 400, 1200, 601};
+        int[] in3951y = {300, 100, 100, 200, 120, 200, 300, 600, 601};
+        int[] in3951width = {250, 350, 200, 1, 0, 100, 420, 50, 51};
+        int[] in3951height = {250, 100, 300, 100, 180, 0, 100, 100, 101};
+        for (int i = 0; i < in3951x.length; i++) {
+            try {
+                task3951(in3951x[i], in3951y[i], in3951width[i], in3951height[i], i);
+            } catch (IllegalArgumentException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
 
+    private static void step7343() {
+        System.out.println("7343");
+        int[] in7343x = {200, 150, 200, 250, 200, 700, 300, 25, 700, -10, 100};
+        int[] in7343y = {200, 180, 200, 250, 120, 100, 300, 25, 500, 810, 650};
+        int[] in7343width = {300, 120, 100, 0, 120, 150, 200, 100, 99, 100, 80};
+        int[] in7343height = {100, 300, 100, 100, 0, 200, 200, 100, 102, 100, 80};
+        int[] in7343r = {40, 50, 120, 20, 25, 20, 0, 50, 3, 100, 45};
+        for (int i = 0; i < in7343x.length; i++) {
+            try {
+                task7343(in7343x[i], in7343y[i], in7343width[i], in7343height[i], in7343r[i], i);
+            } catch (IllegalArgumentException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    private static void step7060() {
         System.out.println("7060");
         int[] in7060x1 = {10, 25, 500, 60, 100, 150, 100, 5, 200, 102, 105, 150, 0};
         int[] in7060y1 = {10, 25, 200, 70, 100, 150, 0, 0, 200, 200, 5, 150, 0};
@@ -63,14 +73,44 @@ public class Lab10 {
         int[] in7060h2 = {350, 200, 120, 200, 50, 300, 100, 10, 10, 120, 30, 200, 610};
         for (int i = 0; i < in7060x1.length; i++) {
             try {
-                task7060(in7060x1[i], in7060y1[i], in7060w1[i], in7060h1[i], in7060x2[i], in7060y2[i], in7060w2[i], in7060h2[i], i);
+//                System.out.printf("<tr>\n" +
+//                    "    <td></td>\n" +
+//                    "    <td>%d</td>\n" +
+//                    "    <td>%d</td>\n" +
+//                    "    <td>%d</td>\n" +
+//                    "    <td>%d</td>\n" +
+//                    "    <td>%d</td>\n" +
+//                    "    <td>%d</td>\n" +
+//                    "    <td>%d</td>\n" +
+//                    "    <td>%d</td>\n", in7060x1[i], in7060y1[i], in7060w1[i], in7060h1[i], in7060x2[i], in7060y2[i], in7060w2[i], in7060h2[i]);
+                task7060(in7060x1[i], in7060y1[i], in7060w1[i], in7060h1[i], in7060x2[i], in7060y2[i], in7060w2[i], in7060h2[i], i + 1);
+//                System.out.println("    <td><img src=\"images/task7060/test" + (i + 1) + ".png\" class=\"example_image\"/></td>");
+            } catch (IllegalArgumentException ex) {
+//                System.out.print("    <td>");
+                System.out.print(ex.getMessage());
+//                System.out.println("</td>");
+            }
+//            System.out.println("</tr>");
+        }
+    }
+
+    private static void step6409() {
+        System.out.println("6409");
+        int[] in6409x = {5, 300, 100, 25, 100, 600, 200, 200, 807};
+        int[] in6409y = {5, 20, 200, 25, 102, 100, 200, 640, 120};
+        int[] in6409a = {500, 20, 200, 0, 200, 210, 200, 120, 200};
+        int[] in6409l = {300, 200, 200, 110, 0, 100, 100, 240, 300};
+        for (int i = 0; i < in6409a.length; i++) {
+            try {
+                task6409(in6409x[i], in6409y[i], in6409a[i], in6409l[i], i + 1);
             } catch (IllegalArgumentException ex) {
                 System.out.println(ex.getMessage());
             }
         }
     }
 
-    private static void writeToFile(String filename, String content) {
+    private static void writeToFile(String fn, String content) {
+        String filename = "out/" + fn;
         try (PrintWriter writer = new PrintWriter(filename, "UTF-8")) {
             writer.println("<!DOCTYPE html>" +
                 "<head>\n" +
@@ -89,6 +129,37 @@ public class Lab10 {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void task6409(int x, int y, int a, int l, int i) {
+        if (a == 0) {
+            throw new IllegalArgumentException("Длина основания должна быть положительна");
+        }
+        if (l == 0) {
+            throw new IllegalArgumentException("Длина боковой стороны должна быть положительна");
+        }
+        if (a >= l * 2) {
+            throw new IllegalArgumentException("Невозможно сформировать треугольник с данными длинами сторон");
+        }
+        int x1 = x + a;
+        int y1 = y;
+        int half = a / 2;
+        int x2 = x + half;
+        int y2 = y + (int) Math.sqrt(l * l - half * half);
+        if(x > VIEWPORT_WIDTH || x1 > VIEWPORT_WIDTH || x2 > VIEWPORT_WIDTH){
+            throw new IllegalArgumentException("Фигура не помещается внутри рисунка");
+        }
+        if(y > VIEWPORT_HEIGHT || y1 > VIEWPORT_HEIGHT || y2 > VIEWPORT_HEIGHT){
+            throw new IllegalArgumentException("Фигура не помещается внутри рисунка");
+        }
+        String lineString = "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"blue\" stroke-width=\"1\"/>";
+        String line1 = String.format(lineString, x, y, x1, y1);
+        String line2 = String.format(lineString, x, y, x2, y2);
+        String line3 = String.format(lineString, x1, y1, x2, y2);
+        String filename = "task6409_" + i + ".html";
+        String result = line1 + line2 + line3;
+        System.out.println(result);
+        writeToFile(filename, result);
     }
 
     public static void task9300(int cx, int cy, int r, int strokeWidth, int testId) {
@@ -172,10 +243,10 @@ public class Lab10 {
     public static void task7060(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2, int testId) {
         String filename = "task7060_" + testId + ".html";
         String rectangleString = "<rect x=\"%d\" y=\"%d\" width=\"%d\" height=\"%d\" fill=\"%s\" />\"";
-        if (x1 <= 0 || x2 <= 0) {
+        if (x1 < 0 || x2 < 0) {
             throw new IllegalArgumentException("Левая сторона прямоугольника должна быть в пределах рисунка");
         }
-        if (y1 <= 0 || y2 <= 0) {
+        if (y1 < 0 || y2 < 0) {
             throw new IllegalArgumentException("Верхняя сторона прямоугольника должна быть в пределах рисунка");
         }
         if (w1 <= 0 || w2 <= 0) {
@@ -192,8 +263,8 @@ public class Lab10 {
         }
         Lab05.Rectangle r1 = Lab05.makeRectangleByXYHW(x1, y1, h1, w1);
         Lab05.Rectangle r2 = Lab05.makeRectangleByXYHW(x2, y2, h2, w2);
-        String result = String.format(rectangleString, x1, y1, w1, h1, "red");
-        result += String.format(rectangleString, x2, y2, w2, h2, "blue");
+        String result = String.format(rectangleString, x1, y1, w1, h1, "blue");
+        result += String.format(rectangleString, x2, y2, w2, h2, "red");
         if (Lab05.task1438check(r1, r2)) {
             Lab05.Rectangle r3 = Lab05.task1438intersect(r1, r2);
             result += String.format(rectangleString, r3.x, r3.y, r3.w, r3.h, "yellow");
