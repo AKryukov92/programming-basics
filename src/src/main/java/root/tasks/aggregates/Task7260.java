e89a4981b0b635d56f18baeea0c6a67e4fc83dca
akryukov92 <akryukov992@gmail.com>
2022-09-19 19:46:23 +0600

    move 7260 and 7290 to coded tasks

--- src/src/main/java/root/tasks/aggregates/Task7260.java
+++ src/src/main/java/root/tasks/aggregates/Task7260.java
@@ -35,13 +35,21 @@ public class Task7260 extends OneInputValLayout {
         appendHeader();
         appendTaskDesc("Пользователь вводит данные - целые числа, разделенные пробелами. Условно разделить числа на группы по 3. Посчитать сумму в каждой группе чисел.");
         appendCheckValuesHeader("data");
-        appendCheckValuesRow("9 8 7 6 5 4");
-        appendCheckValuesRow("10 11 12 20 21 22");
-        appendCheckValuesRow("10 98 62 37 50 41 77 2");
+        appendCheckValuesRow("161 216 143");
+        appendCheckValuesRow("88 54 70 71 86 98");
+        appendCheckValuesRow("10 11 12 20 21 22 95 96 89");
         appendCheckValuesRow("-10 -13 -18 -8 -8 -18 -9 -17 -1 -16 1 1");
         appendCheckValuesRow("258 115 110 192 173 205 246 248 180 127 237 264 247 291 114 ");
-        appendCheckValuesRow("5 9 7 2");
-        appendCheckValuesRow("-3 -6 -8 -20 -4 -90");
+
+        appendCheckValuesRow("1000 2000");
+        appendCheckValuesRow("170 198 265 186 265");
+        appendCheckValuesRow("10 98 62 37 50 41 77 2");
+        appendCheckValuesRow("19 28 23 16 10 13 23 27 21 13 28");
+
+        appendCheckValuesRow("999");
+        appendCheckValuesRow("74 72 76 90");
+        appendCheckValuesRow("-3 -6 -8 -20 -4 -90 0");
+        appendCheckValuesRow("74 72 76 90 86 66 85 65 63 74");
         appendCheckValuesRow("");
         appendCheckValuesFooter();
         appendFooter();
